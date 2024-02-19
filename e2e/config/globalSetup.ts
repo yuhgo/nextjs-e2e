510462d3787e5377943bee43e2438192cba26e15
ゆうご <yuhgo090764@gmail.com>
2024-02-19 17:19:11 +0900

    Add user creation and session setup in globalSetup.ts

--- e2e/config/globalSetup.ts
+++ e2e/config/globalSetup.ts
@@ -1,38 +1,41 @@
 import path from "node:path";
+import { prisma } from "@/_lib/prisma";
 import { chromium } from "@playwright/test";
-// import prisma from "../../lib/prisma";
 
 const globalConfig = async () => {
 	const storagePath = path.resolve(__dirname, "storageState.json");
 	const date = new Date();
 	const sessionToken = "9468389e-ff19-4eb4-bf73-b56516e9b7e8";
 
-	// await prisma.user.upsert({
-	// 	where: {
-	// 		email: "udemy@test.com",
-	// 	},
-	// 	create: {
-	// 		name: "userA",
-	// 		email: "udemy@test.com",
-	// 		sessions: {
-	// 			create: {
-	// 				expires: new Date(date.getFullYear(), date.getMonth() + 6, date.getDate()),
-	// 				sessionToken,
-	// 			},
-	// 		},
-	// 		accounts: {
-	// 			create: {
-	// 				type: "oauth",
-	// 				provider: "github",
-	// 				providerAccountId: "1234567",
-	// 				access_token: "Q3v00dCoWcdzmxpGeiPG2I3wDgsJypSP",
-	// 				token_type: "bearer",
-	// 				scope: "read:user,user:email",
-	// 			},
-	// 		},
-	// 	},
-	// 	update: {},
-	// });
+	await prisma.user.upsert({
+		where: {
+			email: "udemy@test.com",
+		},
+		create: {
+			name: "userA",
+			email: "udemy@test.com",
+			sessions: {
+				create: {
+					expires: new Date(date.getFullYear(), date.getMonth() + 6, date.getDate()),
+					sessionToken,
+				},
+			},
+			accounts: {
+				create: {
+					type: "oauth",
+					provider: "github",
+					providerAccountId: "1234567",
+					// biome-ignore lint/style/useNamingConvention: <explanation>
+					access_token: "Q3v00dCoWcdzmxpGeiPG2I3wDgsJypSP",
+					// biome-ignore lint/style/useNamingConvention: <explanation>
+					token_type: "bearer",
+					scope: "read:user,user:email",
+				},
+			},
+		},
+		update: {},
+	});
+
 	const browser = await chromium.launch();
 	const context = await browser.newContext();
 	await context.addCookies([
